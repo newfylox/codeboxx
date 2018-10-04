56430be80f90c8bb7e60c4c64c8615e22e981284
Philippe Bouillon <newfylox@hotmail.com>
2018-10-04 18:01:55 -0400

    test migration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,7 @@ server "34.203.94.126", user: "ubuntu", roles: %w{app db web}
 set :rails_env, "production"
 set :deploy_to, "/home/ubuntu/apps/newfylox"
 set :linked_dirs, %w{tmp/pids tmp/sockets log}
-set :linked_files, %w{config/master.key}
+set :linked_files, %w{config/secrets.yml config/database.yml}
 
 # role-based syntax
 # ==================
